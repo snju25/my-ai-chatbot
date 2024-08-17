37e243da6745106d6412a749a608223fd7d220b5
snju25 <bhandarisanjay879@gmail.com>
2024-08-17 11:22:35 +1000

    chatbot footer

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <footer>
+        <div style={{width: "100%",padding:20,minHeight:"20vh",maxHeight:"30vh", marginTop: 50}}>
+            <p style={{fontSize:"30px", textAlign:"center"}}>
+                Built by <span className="nav-link" style={{margin: "0px", padding:0}}>Sanjay Bhandari</span>
+            </p>
+        </div>
+    </footer>
+  )
+}
+export default Footer
--- frontend/src/pages/HomeLayout.tsx
+++ frontend/src/pages/HomeLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
 import { useAuth } from "../context/AuthContext"
+import Footer from "../components/footer/Footer"
 
 const HomeLayout = () => {
   return (
     <main>
         <Header/>
         <Outlet/>
+        <Footer/>
     </main>
   )
 }
