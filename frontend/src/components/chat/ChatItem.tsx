24e44115e01429efd9d7ab76e34ffd145dcddf72
snju25 <bhandarisanjay879@gmail.com>
2024-08-21 08:01:26 +1000

    fixed typescirpt error

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Avatar, Box, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coldarkCold, coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 // Extract code blocks and languages from the message
 function extractCodeFromString(message: string) {
--- frontend/src/pages/HomeLayout.tsx
+++ frontend/src/pages/HomeLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
-import { useAuth } from "../context/AuthContext"
 
 const HomeLayout = () => {
   return (
